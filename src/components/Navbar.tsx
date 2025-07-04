a240d5fab7684fd76eb494e154c2d617d6fbdb5d
kiahvaidya <139949621+kiahvaidya@users.noreply.github.com>
2025-07-04 17:25:15 +0530

    Update Navbar.tsx

--- src/components/Navbar.tsx
+++ src/components/Navbar.tsx
@@ -13,8 +13,9 @@ const Navbar = () => {
     { name: 'Home', href: '#home' },
     { name: 'About', href: '#about' },
     { name: 'Projects', href: '#projects' },
-    { name: 'Resume', href: '#resume' },
+    { name: 'Certfications', href: '#courses' },
     { name: 'Leadership', href: '#leadership' },
+    { name: 'Resume', href: '#resume' },
     { name: 'Contact', href: '#contact' },
   ];
 
