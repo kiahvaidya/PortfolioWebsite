816fb5df67cb5b980df6903d1a0e2c1dd40df652
kiah <139949621+kiahvaidya@users.noreply.github.com>
2025-07-06 21:57:46 +0530

    Update Index.tsx

--- src/pages/Index.tsx
+++ src/pages/Index.tsx
@@ -64,7 +64,7 @@ const Index = () => {
     },
      {
       title: "Flight Fare Prediction",
-      description: "This project uses machine learning to predict flight fares based on airline, source, destination, duration, and other flight-related features. bot that replies with a random meme from Reddit when you type $meme.",
+      description: "This project uses machine learning to predict flight fares based on airline, source, destination, duration, and other flight-related features.",
       technologies: ["Machine Learning","Jupyter Notebook"],
       githubLink: "https://github.com/kiahvaidya/FlightFarePrediction"
     }
